2dc6c313fb1d03b8a3cd22e18408ce5457dc8b95
Nguyen Cong Chinh <94957089+NguyenCongCh@users.noreply.github.com>
2023-04-12 12:35:07 +0700

    Them Validation

--- WebCafe/Models/DanhMucSp.cs
+++ WebCafe/Models/DanhMucSp.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebCafe.Models;
 
 public partial class DanhMucSp
 {
+    [Key]
     public int MaDm { get; set; }
 
+    [Required(ErrorMessage = "Tên Danh Mục không được để trống.")]
     public string TenDm { get; set; } = null!;
 
     public string? AnhDm { get; set; }
